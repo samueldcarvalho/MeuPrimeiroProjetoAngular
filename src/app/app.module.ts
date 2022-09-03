069930649ef62328f2c1bd36f61afd8069825f86
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-09-03 18:22:02 -0300

    Criação de módulo de roteamento e primeiras rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { rootRouterConfig } from "./app.routes";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
@@ -6,6 +7,8 @@ import { HeaderComponent } from "./layouts/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { ContactComponent } from "./pages/contact/contact.component";
 import { AboutComponent } from "./pages/about/about.component";
+import { RouterModule } from "@angular/router";
+import { APP_BASE_HREF } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -15,8 +18,11 @@ import { AboutComponent } from "./pages/about/about.component";
     HomeComponent,
     ContactComponent,
   ],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
